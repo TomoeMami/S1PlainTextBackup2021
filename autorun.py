6e76e2f123047af5658609541fda5781e9392519
TomoeMami <trembleafterme@outlook.com>
2021-09-27 15:16:19 +0800

    上传于 2021年09月27日 15:16

--- autorun.py
+++ autorun.py
@@ -93,9 +93,7 @@ if __name__ == '__main__':
     with open(rootdir+'RefreshingData.json',"r",encoding='utf-8') as f:
             thdata=json.load(f)
     for i in thdata.keys():
-        if(thdata[i]['active']) or ((int(time.time()) -thdata[i]['lastedit']) < 2592000) :
+        if(thdata[i]['active']) or ((int(time.time()) -thdata[i]['lastedit']) < 2592000) or (int(threadid) > old_number):
             activethdata[i] = thdata[i]
     with open(rootdir+'RefreshingData.json',"w",encoding='utf-8') as f:
             f.write(json.dumps(activethdata,indent=2,ensure_ascii=False))
-        
-        
